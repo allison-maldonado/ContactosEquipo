0867d676db7e2417e02117cd1f7b101ed52501a7
allison-maldonado <20253ds096@utez.edu.mx>
2026-07-03 08:33:28 -0600

    Commit del servlet

--- src/main/java/mx/edu/utez/contactos/servlet/ContactoServlet.java
+++ src/main/java/mx/edu/utez/contactos/servlet/ContactoServlet.java
@@ -1,12 +1,11 @@
-package mx.edu.utez.integradora_poo_2026.controller;
+package mx.edu.utez.contactos.servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import mx.edu.utez.integradora_poo_2026.model.Contacto;
-import mx.edu.utez.integradora_poo_2026.model.dao.ContactoDao;
+import mx.edu.utez.contactos.model.Contacto;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,12 +13,10 @@ import java.util.List;
 @WebServlet(name = "ContactoServlet", value = "/contacto")
 public class ContactoServlet extends HttpServlet {
 
-    private final ContactoDao contactoDao = new ContactoDao();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<Contacto> lista = contactoDao.getAll();
         request.setAttribute("listaContactos", lista);
         // Apunta a index.jsp tal como lo solicitó el profesor
         request.getRequestDispatcher("index.jsp").forward(request, response);
@@ -40,15 +37,9 @@ public class ContactoServlet extends HttpServlet {
 
         // Creamos el objeto del modelo Contacto
         Contacto nuevoContacto = new Contacto();
-        nuevoContacto.setNombre(nombre);
-        nuevoContacto.setApellidos(apellidos);
-        nuevoContacto.setTelefono(telefono);
-        nuevoContacto.setTelefonoAlt(telefonoAlt);
-        nuevoContacto.setCorreo(correo);
-        nuevoContacto.setRedSocial(redSocial);
+
 
         // Guardamos en la base de datos a través del DAO
-        contactoDao.create(nuevoContacto);
 
         // Redirecciona al mismo servlet (doGet) para actualizar la lista
         response.sendRedirect("contacto");
